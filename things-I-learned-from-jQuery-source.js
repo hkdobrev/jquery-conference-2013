a52867689332d1850410357ea75fe041bf52e750
Haralan Dobrev <hkdobrev@gmail.com>
2013-11-24 18:49:45 +0200

    Initial commit

---
+++ things-I-learned-from-jQuery-source.js
@@ -0,0 +1,140 @@
+// jQuery Bulgaria Conference 2013
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// optimizing Array.prototype.<function>.call with core_deletedIds
+// optimizing Object.prototype.<function>.call with class2type
+var core_concat = core_deletedIds.concat,
+	core_push = core_deletedIds.push,
+	core_slice = core_deletedIds.slice,
+	core_indexOf = core_deletedIds.indexOf,
+	core_toString = class2type.toString,
+	core_hasOwn = class2type.hasOwnProperty,
+	core_trim = core_version.trim;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// breakdown of
+jQuery.type(); // uses class2type
+
+
+
+
+
+
+
+
+
+
+
+jQuery.readyWait++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+jQuery.globalEval;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+jQuery.guid
+
+
+
+
+
+
+
+
+
+
+
+// building with AMD and Grunt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+jQuery.ajaxTransport();
+
+// show example in jQuery-File-Upload
+// https://github.com/blueimp/jQuery-File-Upload/blob/master/js/jquery.iframe-transport.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// All jQuery hooks
+// http://stackoverflow.com/questions/15494498/which-hooks-are-provided-in-jquery/15511578#15511578
