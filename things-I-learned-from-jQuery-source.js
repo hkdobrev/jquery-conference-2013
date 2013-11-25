dc44e284ab9a8bf46540f7970383ba9286734bce
Haralan Dobrev <hkdobrev@gmail.com>
2013-11-25 08:32:33 +0200

    Reduce space to reorder easier. Add more stuff.

--- things-I-learned-from-jQuery-source.js
+++ things-I-learned-from-jQuery-source.js
@@ -1,21 +1,17 @@
-// jQuery Bulgaria Conference 2013
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"  _______________                                        
+"  ______(_)_  __ \___  __________________  __            
+"  _____  /_  / / /  / / /  _ \_  ___/_  / / /            
+"  ____  / / /_/ // /_/ //  __/  /   _  /_/ /             
+"  ___  /  \___\_\\__,_/ \___//_/    _\__, /              
+"  /___/                             /____/               
+"                                                         
+"                                                         
+"  ________       ______                     _____        
+"  ___  __ )___  ____  /______ ______ __________(_)_____ _
+"  __  __  |  / / /_  /__  __ `/  __ `/_  ___/_  /_  __ `/
+"  _  /_/ // /_/ /_  / _  /_/ // /_/ /_  /   _  / / /_/ / 
+"  /_____/ \__,_/ /_/  _\__, / \__,_/ /_/    /_/  \__,_/  
+"                      //___/                             
 
 
 
@@ -29,112 +25,75 @@ var core_concat = core_deletedIds.concat,
 	core_hasOwn = class2type.hasOwnProperty,
 	core_trim = core_version.trim;
 
-
-
-
-
-
-
-
-
-
-
-
+// ~~~
 
 // breakdown of
 jQuery.type(); // uses class2type
 
-
-
-
-
-
-
-
-
-
+// ~~~
 
 jQuery.readyWait++;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ~~~
 
 jQuery.globalEval;
 
-
-
-
-
-
-
-
-
-
-
-
-
+// ~~~
 
 jQuery.guid
 
-
-
-
-
-
-
-
-
-
+// ~~~
 
 // building with AMD and Grunt
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ~~~
 
 jQuery.ajaxTransport();
 
 // show example in jQuery-File-Upload
 // https://github.com/blueimp/jQuery-File-Upload/blob/master/js/jquery.iframe-transport.js
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ~~~
 
 // All jQuery hooks
 // http://stackoverflow.com/questions/15494498/which-hooks-are-provided-in-jquery/15511578#15511578
+
+// ~~~
+
+// Good way to browse through jQuery source code:
+// http://james.padolsey.com/jquery/
+
+// ~~~
+
+jQuery('.btn').addClass(function(index, currentClass) {
+
+});
+
+jQuery('.btn').toggleClass(function(index, currentClass) {
+
+});
+
+// ~~~
+
+// Node.nodeType
+// https://developer.mozilla.org/en-US/docs/Web/API/Node.nodeType
+
+// ~~~
+
+// Don't create clutter
+// src/event/alias.js
+
+jQuery.each( ("blur focus focusin focusout load resize scroll unload click dblclick " +
+	"mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave " +
+	"change select submit keydown keypress keyup error contextmenu").split(" "), function( i, name ) {
+
+	// Handle event binding
+	jQuery.fn[ name ] = function( data, fn ) {
+		return arguments.length > 0 ?
+			this.on( name, null, data, fn ) :
+			this.trigger( name );
+	};
+});
+
+// ~~~
+
